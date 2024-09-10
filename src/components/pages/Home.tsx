f3b4cd7ba63345c3d8937433937db9f0de7a1e07
Shivam Dubey <118355740+shivam969619@users.noreply.github.com>
2024-09-10 19:38:55 +0530

    Update Home.tsx

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,15 +1,1 @@
-import React from "react";
-import { Button } from "../ui/button";
-// Ensure this path is correct
 
-const Home = () => {
-  return (
-    <div>
-      {/* Corrected variant prop */}
-      <div>hello</div>
-      <Button variant="default">Click me</Button>
-    </div>
-  );
-};
-
-export default Home;
